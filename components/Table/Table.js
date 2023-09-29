58f8f93e3126e81abb8109af7b3088bc2fa159a6
Anton <ant1kow22@gmail.com>
2023-09-29 15:42:33 +0300

    react15

--- components/Table/Table.js
+++ components/Table/Table.js
@@ -1,54 +1,68 @@
-class Table extends React.Component {
-  render() {
-    const { elements } = this.props;
-    return (
-      <div>
-        <h4 className="title">
-          {this.props.name}
-        </h4>
-        <table className="table">
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>Name</th>
-              <th>Place</th>
-              <th>Organizer Name</th>
-              <th>Number of Owners</th>
-              <th>Image</th>
-            </tr>
-          </thead>
-          <tbody>
-            {elements.map(element => (
-              <tr key={element.id}>
-                <td>{element.id}</td>
-                <td>{element.name}</td>
-                <td>{element.place}</td>
-                <td>{element.organizerName}</td>
-                <td>{element.numberOfOwners}</td>
-                <td><img src={`./data/img/${element.img}`}
-                  className="event_img"
-                  alt={element.name} /></td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+var Table = React.createClass({
+  displayName: "Table",
+  render: function () {
+    return React.createElement(
+      "div",
+      null,
+      React.createElement(
+        "h4",
+        { className: "title" },
+        this.props.name
+      ),
+      React.createElement(
+        "table",
+        { className: "table" },
+        React.createElement(
+          "thead",
+          null,
+          React.createElement(
+            "tr",
+            null,
+            React.createElement("th", null, "ID"),
+            React.createElement("th", null, "Name"),
+            React.createElement("th", null, "Place"),
+            React.createElement("th", null, "Organizer Name"),
+            React.createElement("th", null, "Number of Owners"),
+            React.createElement("th", null, "Image")
+          )
+        ),
+        React.createElement(
+          "tbody",
+          null,
+          this.props.elements.map(function (element) {
+            return React.createElement(
+              "tr",
+              { key: element.id },
+              React.createElement("td", null, element.id),
+              React.createElement("td", null, element.name),
+              React.createElement("td", null, element.place),
+              React.createElement("td", null, element.organizerName),
+              React.createElement("td", null, element.numberOfOwners),
+              React.createElement("td", null, React.createElement("img", {
+                src: "./data/img/" + element.img,
+                className: "event_img",
+                alt: element.name
+              }))
+            );
+          })
+        )
+      )
     );
-  }
-
-  static propTypes = {
-    elements: React.PropTypes.arrayOf(
-      React.PropTypes.shape({
-        id: React.PropTypes.number.isRequired,
-        name: React.PropTypes.string.isRequired,
-        place: React.PropTypes.string.isRequired,
-        organizerName: React.PropTypes.string.isRequired,
-        numberOfOwners: React.PropTypes.number.isRequired,
-        img: React.PropTypes.string.isRequired,
-      })
-    ).isRequired,
-    name: React.PropTypes.string.isRequired,
-  };
+  },
+});
 
 
-}
+//   static propTypes = {
+//     elements: React.PropTypes.arrayOf(
+//       React.PropTypes.shape({
+//         id: React.PropTypes.number.isRequired,
+//         name: React.PropTypes.string.isRequired,
+//         place: React.PropTypes.string.isRequired,
+//         organizerName: React.PropTypes.string.isRequired,
+//         numberOfOwners: React.PropTypes.number.isRequired,
+//         img: React.PropTypes.string.isRequired,
+//       })
+//     ).isRequired,
+//     name: React.PropTypes.string.isRequired,
+//   };
+// }
