c5f4280db47844be2ba0a131045012a6197e4b02
Anton <ant1kow22@gmail.com>
2023-10-18 12:44:32 +0300

    asdasd

--- components/Table/Table.js
+++ components/Table/Table.js
@@ -15,22 +15,20 @@ var Table = React.createClass({
             null,
             React.DOM.th(null, "ID"),
             React.DOM.th(null, "Name"),
-            React.DOM.th(null, "Place"),
-            React.DOM.th(null, "Organizer Name"),
-            React.DOM.th(null, "Number of Owners"),
+            React.DOM.th(null, "Salary"),
+            React.DOM.th(null, "Organization"),
             React.DOM.th(null, "Image")
           )
         ),
         React.DOM.tbody(
           null,
-          this.props.elements.map(function (element) {
+          this.props.vacancys.map(function (element) {
             return React.DOM.tr(
               { key: element.id },
               React.DOM.td(null, element.id),
               React.DOM.td(null, element.name),
-              React.DOM.td(null, element.place),
-              React.DOM.td(null, element.organizerName),
-              React.DOM.td(null, element.numberOfOwners),
+              React.DOM.td(null, element.salary),
+              React.DOM.td(null, element.organization),
               React.DOM.td(
                 null,
                 React.DOM.img({
